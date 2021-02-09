30ec375ea9f57edb422dd5aaa73cfffc7bf64448
Shinobi Gakiya <vgakiya@better.com.mx>
2021-02-08 19:53:23 -0600

    some threads

---
+++ src/com/timbuchalka/Main.java
@@ -0,0 +1,45 @@
+package com.timbuchalka;
+
+import static com.timbuchalka.ThreadColor.ANSI_GREEN;
+import static com.timbuchalka.ThreadColor.ANSI_PURPLE;
+import static com.timbuchalka.ThreadColor.ANSI_RED;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println(ANSI_PURPLE + "Hello from the main thread.");
+
+    Thread anotherThread = new AnotherThread();
+    anotherThread.setName("== Another Thread ==");
+    anotherThread.start();
+
+    new Thread() {
+      public void run() {
+        System.out.println(ANSI_GREEN + "Hello from the anonymous class thread");
+      }
+    }.start();
+
+    Thread myRunnableThread = new Thread(new MyRunnable() {
+      @Override
+      public void run() {
+        System.out.println(ANSI_RED + "Hello from the anonymous class's implementation of run()");
+
+        try{
+          anotherThread.join(2000);
+          System.out.println(ANSI_RED + "AnotherThread terminated or timed out, so I'm running again");
+        } catch (InterruptedException e){
+          System.out.println(ANSI_RED + "I couldn't wait, I was interrupted");
+        }
+
+      }
+    });
+
+    myRunnableThread.start();
+
+    // anotherThread.interrupt();
+
+    System.out.println(ANSI_PURPLE + "Hello again from the main thread.");
+
+
+  }
+}
